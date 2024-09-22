4f5642d303f6bea59d277f192c0cd9171d2a60bb
Rand1008 <159923712+Rand1008@users.noreply.github.com>
2024-09-22 21:07:30 +0800

    script.js

--- script.js
+++ script.js
@@ -1,71 +1,51 @@
 firebase.initializeApp({
-  apiKey: 'AIzaSyBIrOPXXekMWli0y-0NvQ5KmWQ2kssJrNw',
-  authDomain: 'data-b738a.firebaseapp.com',
-  databaseURL: 'https://data-b738a-default-rtdb.firebaseio.com',
-  projectId: 'data-b738a',
-  storageBucket: 'data-b738a.appspot.com',
-  messagingSenderId: '390422282553',
-});
+    apiKey: 'AIzaSyBIrOPXXekMWli0y-0NvQ5KmWQ2kssJrNw',
+    authDomain: 'data-b738a.firebaseapp.com',
+    databaseURL: 'https://data-b738a-default-rtdb.firebaseio.com',
+    projectId: 'data-b738a',
+    storageBucket: 'data-b738a.appspot.com',
+    messagingSenderId: '390422282553',
+    });
 
-var database = firebase.database();
+    var database = firebase.database();
 
-var form = document.getElementById('contactForm');
-var nameInput = document.getElementById('name');
-var emailInput = document.getElementById('email');
-var studIdInput = document.getElementById('studId');
-var msgContentInput = document.getElementById('MsgContent');
+    var form = document.getElementById('contactForm');
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('email');
+    var studIdInput = document.getElementById('studId');
+    var msgContentInput = document.getElementById('MsgContent');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+    form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  var name = nameInput.value;
-  var email = emailInput.value;
-  var studId = studIdInput.value;
-  var msgContent = msgContentInput.value;
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var studId = studIdInput.value;
+    var msgContent = msgContentInput.value;
 
-  var timestamp = new Date().toLocaleString();
-  
-  var newData = {
-    name: name,
-    email: email,
-    studId: studId,
-    msgContent: msgContent,
-    timestamp: timestamp,
-  };
+    var timestamp = new Date().toLocaleString();
 
-  database.ref('messages').push(newData);
+    var newData = {
+        name: name,
+        email: email,
+        studId: studId,
+        msgContent: msgContent,
+        timestamp: timestamp,
+    };
 
-  document.querySelector('.alert').innerHTML = 'Your message has been sent!';
-  document.querySelector('.alert').style.display = 'block';
-  form.reset()
-});
+    database.ref('messages').push(newData);
 
-document.getElementById('open-btn').addEventListener('click', function() {
-  document.getElementById('contact-container').style.display = 'block';
-  document.getElementById('overlay').style.display = 'block';
-});
+    document.querySelector('.alert').innerHTML = 'Your message has been sent!';
+    document.querySelector('.alert').style.display = 'block';
+    form.reset()
+    });
 
-document.getElementById('overlay').addEventListener('click', function() {
-  document.getElementById('contact-container').style.display = 'none';
-  document.getElementById('overlay').style.display = 'none';
-});
+    document.getElementById('open-btn').addEventListener('click', function() {
+    document.getElementById('contact-container').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+    });
 
-var images = [
-            'sam.jpg', // Replace with your first image path
-            'download.jfif',
-            'Robin.jpg',
-            'stellfly.png',
-            'animation.gif'
-        ];
-
-        // Current image index
-        var currentIndex = 0;
-
-        // Function to change background image every 20 seconds
-        function changeBackground() {
-            currentIndex = (currentIndex + 1) % images.length; // Cycle through the images array
-            document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
-        }
-
-        // Call changeBackground function every 20 seconds (20000 milliseconds)
-        setInterval(changeBackground, 10000);
+    document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('contact-container').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+    });
